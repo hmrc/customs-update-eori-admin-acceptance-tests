c30a7f460bd0e90298d5bb290f9d732041709a95
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-03-06 10:42:35 +0000

    remove duplicate dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,6 @@ object Dependencies {
     "org.scalatest"                 %% "scalatest"      % "3.2.18"   % Test,
     "org.scalatestplus"             %% "selenium-4-12"  % "3.2.17.0" % Test,
     "uk.gov.hmrc"                   %% "ui-test-runner" % "0.21.0"   % Test,
-    "com.typesafe"                   % "config"         % "1.4.2"    % Test,
     "com.softwaremill.sttp.client4" %% "core"           % "4.0.0-M1" % Test
   )
 
