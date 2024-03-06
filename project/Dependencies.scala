224803698a3e6049a1524f8e0139093b7b0c7ca8
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-03-06 15:11:41 +0000

    upgrade typesafe lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "com.typesafe"                   % "config"         % "1.4.2"    % Test,
+    "com.typesafe"                   % "config"         % "1.4.3"    % Test,
     "com.vladsch.flexmark"           % "flexmark-all"   % "0.64.8"   % Test,
     "org.scalatest"                 %% "scalatest"      % "3.2.18"   % Test,
     "org.scalatestplus"             %% "selenium-4-12"  % "3.2.17.0" % Test,
