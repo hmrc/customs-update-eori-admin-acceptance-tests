bd0e42868f73a776ba59b71831abdbdac21a6343
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-06-03 12:06:52 +0100

    upgrade ui test runner

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,11 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
-  val test = Seq(
-    "com.typesafe"                   % "config"         % "1.4.3"    % Test,
-    "com.vladsch.flexmark"           % "flexmark-all"   % "0.64.8"   % Test,
-    "org.scalatest"                 %% "scalatest"      % "3.2.18"   % Test,
-    "org.scalatestplus"             %% "selenium-4-12"  % "3.2.17.0" % Test,
-    "uk.gov.hmrc"                   %% "ui-test-runner" % "0.21.0"   % Test,
-    "com.softwaremill.sttp.client4" %% "core"           % "4.0.0-M1" % Test
-  )
+  val test: Seq[ModuleID] = Seq(
+    "org.scalatestplus"             %% "selenium-4-12"  % "3.2.17.0",
+    "uk.gov.hmrc"                   %% "ui-test-runner" % "0.30.0",
+    "com.softwaremill.sttp.client4" %% "core"           % "4.0.0-M1"
+  ).map(_ % Test)
 
 }
